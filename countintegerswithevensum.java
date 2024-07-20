e8fca335aa3208e7aa76d7013318e66978397a43
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-07-20 23:48:04 +0530

    added countintegerswithevensum

--- countintegerswithevensum.java
+++ countintegerswithevensum.java
@@ -4,30 +4,29 @@
 
 // Testing my logic on local machine.
 public class countintegerswithevensum {
+    
     public static void main(String args[]) {
+        int count=0;
         int n = 30;
         int sum=0;
-        int count=0;
 
         for(int i=1;i<=n;i++)
         {   int temp=i;
             sum=0;
             int rem=0;
+            
             while(temp!=0)
             {   
-                rem=i%10;
+                rem=temp%10;
                 sum=sum+rem;
                 temp=temp/10;
             }
-
-            if(sum%2==0)
+            if(sum%2==0 && sum!=0)
             {   
                 count++;
-                System.out.print(i+" ");
-                //System.out.print(sum);
             }
         }
-        //System.out.print(count);
         
+        System.out.println(count);
     }
 }
