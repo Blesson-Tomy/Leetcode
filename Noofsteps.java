05a35bb1c0dc325fcd645108417fd0efb1b1e3e6
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-08-19 23:46:31 +0530

    added Number of Steps to reduce a number to Zero

--- Noofsteps.java
+++ Noofsteps.java
@@ -1,3 +1,7 @@
+// Given an integer num, return the number of steps to reduce it to zero.
+//In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+//Leetcode Submission:
 class Solution {
     public int numberOfSteps(int num) {
         int count=0;
