31be2b177d9cfb67f23c176daa8f1d0345d05232
Blesson K Tomy <blessonsmac@gmail.com>
2025-12-12 23:10:50 +0530

    added FirstUniqueCharacterInaString.java

---
+++ FirstUniqueCharacterInaString.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        char an=' ';
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {   char c = s.charAt(i);
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
